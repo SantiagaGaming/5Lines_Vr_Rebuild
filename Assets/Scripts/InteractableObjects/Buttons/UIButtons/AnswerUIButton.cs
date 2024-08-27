d8abe8915031601842e4150911c123f64c2d7786
Alexander <sherstoboev93@gmail.com>
2024-08-27 16:54:53 +0300

    add new sp6 model

--- Assets/Scripts/InteractableObjects/Buttons/UIButtons/AnswerUIButton.cs
+++ Assets/Scripts/InteractableObjects/Buttons/UIButtons/AnswerUIButton.cs
@@ -9,27 +9,26 @@ public class AnswerUIButton : BaseUIButton
     [SerializeField] private OtkazAOSUIButton[] _otkazButton;
     [SerializeField] private Text _answerButtonText;
     [SerializeField] private TextMeshProUGUI[] _attempText;
-    private string _buttonId="";
+    private string _buttonId = "";
     private string _attemp = "Осталось попыток: 1";
     public OtkazAOSUIButton[] OtkazButtons => _otkazButton;
     protected override void Click()
     {
-       API.OnReasonInvoke(_buttonId);
-        Debug.Log(_buttonId);
+        API.OnReasonInvoke(_buttonId);
         SetText(_attemp);
-    }    
+    }
 
     public void SetColor()
     {
-        
+
         foreach (var otkazButton in _otkazButton)
         {
             if (otkazButton.Check)
             {
-               
+
                 Button.image.color = new Color(1, 1, 1, 1);
                 Button.enabled = true;
-                _answerButtonText.color = new Color(1,1,1,1);
+                _answerButtonText.color = new Color(1, 1, 1, 1);
                 return;
             }
             else
@@ -39,7 +38,7 @@ public class AnswerUIButton : BaseUIButton
                 _answerButtonText.color = new Color(1, 1, 1, 0.4f);
             }
         }
-        
+
     }
     public void SetId(string id)
     {
@@ -47,7 +46,7 @@ public class AnswerUIButton : BaseUIButton
     }
     private void SetText(string attemptext)
     {
-        foreach(var text in _attempText)
+        foreach (var text in _attempText)
         {
             text.text = attemptext;
         }
