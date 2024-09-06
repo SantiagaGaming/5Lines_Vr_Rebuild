4458e8fcf542e3830364ee3d2c36fac27ebdf4c6
Alexander <sherstoboev93@gmail.com>
2024-09-06 10:44:40 +0300

    add new animation

--- Assets/Scripts/AOSObjects/Sp6BlendShape.cs
+++ Assets/Scripts/AOSObjects/Sp6BlendShape.cs
@@ -6,20 +6,39 @@ using UnityEngine;
 public class Sp6BlendShape : AosObjectBase
 {
     public int BlendShape = 0;
-    public float  BlendShape2 = 100f;
+    public float BlendShape2 = 100f;
     private SkinnedMeshRenderer skinnedMesh;
+    [SerializeField] private SkinnedMeshRenderer _reg1Mesh;
+    [SerializeField] private SkinnedMeshRenderer _reg2Mesh;
     private void Start()
     {
-         skinnedMesh = GetComponent<SkinnedMeshRenderer>();
-        
+        skinnedMesh = GetComponent<SkinnedMeshRenderer>();
+
     }
     public void SetCondition(bool value)
     {
-        Debug.Log("IN");
-        if(value)
-        skinnedMesh.SetBlendShapeWeight(BlendShape, BlendShape2);
+
+        if (value)
+        {
+            skinnedMesh.SetBlendShapeWeight(BlendShape, BlendShape2);
+            if (_reg1Mesh != null && _reg2Mesh != null)
+            {
+                _reg1Mesh.SetBlendShapeWeight(BlendShape, BlendShape2);
+                _reg2Mesh.SetBlendShapeWeight(BlendShape, BlendShape2);
+            }
+
+        }
+
         else
+        {
             skinnedMesh.SetBlendShapeWeight(BlendShape, BlendShape);
+            if (_reg1Mesh != null && _reg2Mesh != null)
+            {
+                _reg1Mesh.SetBlendShapeWeight(BlendShape, BlendShape);
+                _reg2Mesh.SetBlendShapeWeight(BlendShape, BlendShape);
+            }
+        }
+
     }
 }
 
